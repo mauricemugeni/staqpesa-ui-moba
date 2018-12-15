4b671b1836d0339656523cfe6eea5b8ad068e37c
Think Binary. Think Code <hello@reflexconcepts.co.ke>
2018-12-15 08:32:49 +0300

    Merger

--- core/database/include.php
+++ core/database/include.php
@@ -1,5 +1,5 @@
 <?php
-$configs = parse_ini_file(WPATH . "core/configs.ini");
+$configs = parse_ini_file(WPATH . "core/configs_lb.ini");
 require "Database.php";
 Database::setUp(array(
     'dsn' => 'mysql:host=' . $configs["host_name"] . ';dbname=' . $configs["db_name"] . ';',
